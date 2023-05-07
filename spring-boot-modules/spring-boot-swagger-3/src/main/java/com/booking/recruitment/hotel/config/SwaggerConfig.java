8b937f26b95f211d3245c61089dcf5027db40181
abdul0214 <works.abdul@gmail.com>
2023-05-07 19:24:06 +0200

    base application

---
+++ spring-boot-modules/spring-boot-swagger-3/src/main/java/com/booking/recruitment/hotel/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.booking.recruitment.hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String REGEX_API = "/api.+";
+
+    private static ApiInfo apiInfo(String title, String description, String version) {
+        return new ApiInfo(title, description, version, null, null, null, null, new ArrayList<>());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Bank API")
+                .apiInfo(apiInfo("Banking API", "API documentation for backend module of Banking Project", "1.0"))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex(REGEX_API))
+                .build();
+    }
+}
---
+++ spring-boot-modules/spring-boot-swagger-3/src/main/java/com/booking/recruitment/hotel/controller/HotelController.java
@@ -0,0 +1,36 @@
+package com.booking.recruitment.hotel.controller;
+
+import com.booking.recruitment.hotel.model.Hotel;
+import com.booking.recruitment.hotel.service.HotelService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/hotel")
+@Api(tags = "Hotel")
+public class HotelController {
+  private final HotelService hotelService;
+
+  @Autowired
+  public HotelController(HotelService hotelService) {
+    this.hotelService = hotelService;
+  }
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  @ApiOperation(value = "Get Account Statement")
+  public List<Hotel> getAllHotels() {
+    return hotelService.getAllHotels();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Hotel createHotel(@RequestBody Hotel hotel) {
+    return hotelService.createNewHotel(hotel);
+  }
+}
